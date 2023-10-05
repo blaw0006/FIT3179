1ce59482876a4f1a9f531477596a2e224c8d8624
blaw0006 <145531359+blaw0006@users.noreply.github.com>
2023-10-05 11:12:10 +1100

    Some small changes to layout

--- assignment2/js/embedding.js
+++ assignment2/js/embedding.js
@@ -1,4 +1,4 @@
-var spec2 = "js/choropleth_map.vg.json";
-  vegaEmbed('#choropleth_map', spec2).then(function(result) {
+var vis_1 = "js/choropleth_map.vg.json";
+  vegaEmbed('#choropleth_map', vis_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
