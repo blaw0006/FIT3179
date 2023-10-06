892288e264d8ccd9d8381e1dfa6aee294b2df7de
blaw0006 <145531359+blaw0006@users.noreply.github.com>
2023-10-06 22:49:06 +1100

    Added file for interactive map + area chart

--- assignment2/js/embedding.js
+++ assignment2/js/embedding.js
@@ -2,3 +2,8 @@ var vis_1 = "js/choropleth_map.vg.json";
   vegaEmbed('#choropleth_map', vis_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
+
+var vis_2 = "js/area_chart.vg.json";
+  vegaEmbed('#area_chart', vis_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
