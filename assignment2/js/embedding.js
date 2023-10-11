22d76c04535606de52ae5f81dca766325624ac76
blaw0006 <145531359+blaw0006@users.noreply.github.com>
2023-10-12 02:29:06 +1100

    Embedded radial plot, added _ to clean vaccination data

--- assignment2/js/embedding.js
+++ assignment2/js/embedding.js
@@ -7,3 +7,8 @@ var vis_2 = "js/area_chart.vg.json";
   vegaEmbed('#area_chart', vis_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   }).catch(console.error);
+
+var vis_3 = "js/radial_plot.vg.json";
+  vegaEmbed('#radial_plot', vis_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  }).catch(console.error);
